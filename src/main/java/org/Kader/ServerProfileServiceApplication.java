699edf4c4901bb50872203ba816c628d5bd477d3
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-09-03 19:09:58 +0200

    add @EntityScan for ServerModelClassExample

--- src/main/java/org/Kader/ServerProfileServiceApplication.java
+++ src/main/java/org/Kader/ServerProfileServiceApplication.java
@@ -2,8 +2,10 @@ package org.Kader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("org.great.ServerModelClassExample.entities")
 public class ServerProfileServiceApplication {
 
 	public static void main(String[] args) {
